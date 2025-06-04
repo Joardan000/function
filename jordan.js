463c1be27cda90dd57ae3b417333cb489d1b2d7f
Joardan000 <Joardan000@gmail.com>
2025-06-04 15:20:48 +0500

    imtihon js

--- jordan.js
+++ jordan.js
@@ -1,88 +1,68 @@
-/*function Jordan() {
-    let password = prompt("Enter your password");
-    if (password === "salommazgidunyo") {
-        alert("Xush kelibsiz!");
-        console.log(`${password} is true!`);
-    }else {
-        alert("Xush kelmabsiz!");
-        console.log(`${password} is wrong!`);
-    }
-}Jordan();*/
+// let a = prompt("1-sonni kiriting!")
+// let b = prompt("2-sonni kiriting!")
+// let c = prompt("Amal kiriting!")
+// if (c === "+") {
+//     alert(a+b)
+// }else if (c === "-"){
+//     alert(a-b)
+// }else if (c === "*"){
+//     alert(a*b)
+// }else if (c === "/"){
+//     alert(a/c)
+// }else {
+//     alert("Amal kiriting!")
+// }
 
-/*function Jordan() {
-    let ball = prompt("What is the ball?")*1;
-    if (ball >= 90 ) {
-        alert("Yaxshi o'qiykansan hehe!");
-        console.log(`${ball} ball chotki ball!`);
-    }else if (ball > 90 && ball <= 70) {
-        alert("Bo'ladi ukam, o'tding!");
-        console.log(`${ball} ball o'rtacha ball!`);
-    }else if(ball > 70 && ball >=50){
-        alert("Yaxshi o'qisen bumeydimi?");
-        console.log(`${ball} ball bumeydigan ball!`);
-    }else{
-        alert("Yiqilling bratishka!");
-        console.log(`${ball} ball: Yiqilling degani!`);
-    }
-}Jordan();*/
+// let cars1 = [
+//     {
+//         model:"BMW",
+//         cost: "$125000",
+//         km:"100 000km"
+//     },
+//     {
+//         model:"Traktor",
+//         cost: "$12000",
+//         km:"150 000km"
+//     }
+// ]
+// car2 = [
+//     {
+//         model:"BMW",
+//         cost: "$125000",
+//         km:"100 000km"
+//     },
+//     {
+//         model:"Traktor",
+//         cost: "$12000",
+//         km:"150 000km"
+//     }
+// ]
+// let allcars =  cars1.concat(car2)
+// console.log(allcars);
 
-/*function Jordan() {
-    let x = prompt("Hafta kunini kiriting!")
-    if(x === "Dushanba" && "Seshanba" && "Chorshanba" && "Payshanba" && "Juma"){
-        alert("Ishlashing kerak, ro'zg'orni kim qiladi?")
-    }else{
-        alert("Dam ol bratishka, sanam chachchading!")
-    }
-}Jordan()*/
+// let a = prompt("Son kiriting!")*1
+// if( a % 2 === 0 ){
+//     alert("Juft son!")
+// }else{
+//     alert("Toq son!")
+// }
 
-/*function Jordan() {
-    let x = prompt("Ob-havo gradusini kiriting!");
-    if(x >= 30){
-        alert("Issiqku, kettik basseynga!")
-    }else if(x < 30 && x >= 15){
-        alert("Iliq havo ekan, toqqa chiqamizmi?")
-    }else if(x < 15 && x >= 0){
-        alert("Ukam basseynam, tog'am atmen karochi!")
-    }else {
-        alert("Ko'zni qisib uyda o'tiraylik!")
-    }
-}Jordan()*/
+// let n = prompt("Iltimos, biror son kiriting:")*1;
+// for (let i = 2; i <= n; i++) {
+//     let tub = true;
+//     for (let j = 2; j <= Math.sqrt(i); j++) {
+//         if (i % j === 0) {
+//             tub = false;
+//             break;
+//         }
+//     }
+//     if (tub) {
+//         console.log(i);
+//     }
+// }
 
-/*function Jordan() {
-    let a = prompt("1-sonni kiriting!")
-    let b = prompt("2-sonni kiriting!")
-    let c = prompt("3-sonni kiriting!")
-    if (a > b && a > c) {
-        alert("1-son eng kattasi!")
-    }else if (b > c && b > a) {
-        alert("2-son eng kattasi!")
-    }else (
-        alert("3-son eng kattasi!")
-    )
-}Jordan()*/
 
-/*function Jordan() {
-    let a = prompt("1-tomonni kiriting!")*1
-    let b = prompt("2-tomonni kiriting!")*1
-    let c = prompt("3-tomonni kiriting!")*1
-    if ( a===b && b===a && c===b && a===c && b===c && c===a ) {
-        alert("Teng tomonli uchburchak!")
-    }else if ( a===b && b===a || b===c && c===b || c===a && a===c ) {
-        alert("Teng yonli uchburchak!")
-    }else {
-        alert("Turli tomonli!")
-    }
-}Jordan()*/
 
-/*function Jordan() {
-    let x = prompt("Telefon raqamingizni kiriting!");
-    if(x.startsWith("+998")){
-        alert("Uzbekistan raqami!")
-    }else{
-        alert("Boshqa davlat raqami!")
-    }
-}
-Jordan()*/
 
 
 
